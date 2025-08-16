6d84f2a6c086814f68f7ad2f69f64a5c4f56da47
dcj <jekson998@blaster.co.id>
2025-08-16 10:31:39 +0700

    Add preloading image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Features from "./components/Features";
 import FirstView from "./components/FirstView";
 import Footer from "./components/Footer";
 import Meta from "./components/Meta";
+import { useEffect } from "react";
 
 function App() {
   return (
@@ -16,10 +18,38 @@ function App() {
   );
 }
 
+const preLoadImage = (src) => {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve(img);
+  });
+};
+
 function ContentWrapper({ children }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    Promise.all([
+      preLoadImage("hero-2.png"),
+      preLoadImage("vndb-bg.png"),
+      preLoadImage("badge-obtainium.png"),
+      preLoadImage("badge-playstore.png"),
+      preLoadImage("badge-appstore.png"),
+    ]).then(() => {
+      setImageLoaded(true);
+    });
+  });
+
   return (
     <div align="center">
-      <div className="root">{children}</div>
+      {imageLoaded ? (
+        <div className="root">{children}</div>
+      ) : (
+        <p className="absolute top-[50%] left-[50%] -translate-[50%] text-xl ">
+          <span className="size-4 spinner mr-1"></span> Loading...
+        </p>
+      )}
     </div>
   );
 }
